e391ee273feb8010914a22734c94d33b39ca9749
Michael Hoffer <info@michaelhoffer.de>
2013-01-31 16:09:35 +0100

    version updated

--- VRL-Studio/src/eu/mihosoft/vrlstudio/main/Constants.java
+++ VRL-Studio/src/eu/mihosoft/vrlstudio/main/Constants.java
@@ -59,7 +59,7 @@ public class Constants {
 
     public static String APP_NAME = "VRL-Studio";
     // version number
-    public static final String VERSION_BASE = "0.4.5";
+    public static final String VERSION_BASE = "0.4.5.1";
     // "HEAD" or "unstable" etc.
     public static final String VERSION_SUFFIX = "-HEAD";
     // final version string: 
