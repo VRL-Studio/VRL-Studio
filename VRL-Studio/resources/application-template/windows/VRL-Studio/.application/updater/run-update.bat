8942349c156079600bdb0135199bc187e3929f8c
Michael Hoffer <info@michaelhoffer.de>
2013-02-04 20:15:49 +0100

    windows updater should support file paths with spaces now (will test this soon)

--- VRL-Studio/resources/application-template/windows/VRL-Studio/.application/updater/run-update.bat
+++ VRL-Studio/resources/application-template/windows/VRL-Studio/.application/updater/run-update.bat
@@ -1,5 +1,5 @@
 @echo off
-set CONF=-updater %*
+set CONF=-updater
 
 REM set APPDIR="%CD%\.application"
 set APPDIR="%CD%" REM .application folder
@@ -29,6 +29,6 @@ REM version of 2011
 REM start /min /realtime %JAVAEXE% -Xms64m -Xmx512m -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -XX:+CMSPermGenSweepingEnabled -XX:MaxPermSize=256m -splash:resources\studio-resources\splashscreen.png -Djava.library.path="%LIBDIR%" -jar VRL-Studio.jar %CONF%
 
 REM optimized for jre 7 (19.04.2012)
-start /min /realtime %JAVAEXE% -Xms64m -Xmx%MAXHEAP%m -XX:MaxPermSize=256m -splash:resources\studio-resources\splashscreen-update.png -Djava.library.path="%LIBDIR%" -jar VRL-Studio.jar %CONF%
+start /min /realtime %JAVAEXE% -Xms64m -Xmx%MAXHEAP%m -XX:MaxPermSize=256m -splash:resources\studio-resources\splashscreen-update.png -Djava.library.path="%LIBDIR%" -jar VRL-Studio.jar %CONF% "%*"
 
 exit
