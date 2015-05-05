3fe62c2f4c90b9a15d4e9169d9a84201ab7958b5
Michael Hoffer <miho@Michaels-MacBook-Pro.local>
2015-05-05 15:10:18 +0200

    improved run script

--- VRL-Studio/resources/application-template/osx/VRL-Studio.app/Contents/MacOS/run.sh
+++ VRL-Studio/resources/application-template/osx/VRL-Studio.app/Contents/MacOS/run.sh
@@ -11,7 +11,11 @@ CONF="-enable3d yes -resolution 1024x768 -defaultProject default.vrlp -property-
 
 ######### DO NOT EDIT #########
 
-OSX_CONF="-Xdock:icon=../vrl-icon-osx.icns -Djava.library.path=custom-lib/osx -Xbootclasspath/p:lib/java3d/j3dcore.jar:lib/java3d/j3dutils.jar:lib/osx/jogl.jar:lib/java3d/vecmath.jar:lib/osx/gluegen-rt.jar -Dsun.boot.library.path=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Libraries:lib/osx -Dapple.laf.useScreenMenuBar=true"
+#OSX_CONF="-Xdock:icon=../vrl-icon-osx.icns -Djava.library.path=custom-lib/osx -Xbootclasspath/p:lib/java3d/j3dcore.jar:lib/java3d/j3dutils.jar:lib/osx/jogl.jar:lib/java3d/vecmath.jar:lib/osx/gluegen-rt.jar -Dsun.boot.library.path=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Libraries:lib/osx -Dapple.laf.useScreenMenuBar=true"
+
+#OSX_CONF="-Xdock:icon=../vrl-icon-osx.icns -Dapple.laf.useScreenMenuBar=true -Djava.ext.dirs=lib/ext -cp lib/java3d/jogl-all.jar:lib/java3d/gluegen.jar:lib/java3d/gluegen-rt.jar:lib/java3d/j3dcore.jar:lib/java3d/j3dutils.jar:lib/java3d/vecmath.jar:lib/osx/gluegen-rt-natives-macosx-universal.jar:lib/osx/jogl-all-natives-macosx-universal.jar" 
+
+OSX_CONF="-Xdock:icon=../vrl-icon-osx.icns -Dapple.laf.useScreenMenuBar=true  -Djava.ext.dirs=." 
 
 
 
@@ -19,18 +23,25 @@ APPDIR="$(dirname "$0")/../Resources/.application"
 cd "$APPDIR"
 APPDIR="$(pwd)"
 
+if [[ "$(uname)" == "Darwin" ]]
+then
+	cd ../Java
+	
+	echo ">> detected os x: $(pwd)"
+fi
+
 # ugly hack to enable vtk on osx
 export DYLD_LIBRARY_PATH="$HOME/.vrl/$VRL_VERSION/$PROPERTY_SUFFIX/plugins/VRL-VTK/natives/osx/:$DYLD_LIBRARY_PATH"
 
 # IF APPLICATION STUB SHALL BE USED
-../../MacOS/JavaApplicationStub "$@"&
-exit 0
+#../../MacOS/JavaApplicationStub "$@"&
+#exit 0
 
 
 # IF BASH SCRIPT SHALL BE USED
 LIBDIR32="lib/linux/x86:custom-lib/linux/x86"
 LIBDIR64="lib/linux/x64:custom-lib/linux/x64"
-LIBDIROSX="lib/osx:custom-lib/osx"
+LIBDIROSX=""
 
 
 if [[ $OS == *x86_64* ]]
@@ -58,7 +69,7 @@ fi
 
 echo "PARAMS: $@"
 
-$JAVAEXE -Xms64m -Xmx1024m -XX:MaxPermSize=256m -splash:resources/studio-resources/splashscreen.png $OSX_CONF -jar VRL-Studio.jar $CONF "-file" "$@"
+$JAVAEXE -Xms64m -Xmx4096m -splash:resources/studio-resources/splashscreen.png $OSX_CONF -jar VRL-Studio.jar $CONF "-file" "$@"
 
 #PID=$!
 #wait $PID
