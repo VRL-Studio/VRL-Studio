f386b6b98a244606393be978db8753ab97e96bab
Michael Hoffer <info@michaelhoffer.de>
2015-06-30 21:53:34 +0200

    ext dir fixed: find javafx now and ignores old os x jvm extensions

--- VRL-Studio/resources/application-template/osx/VRL-Studio.app/Contents/MacOS/run.sh
+++ VRL-Studio/resources/application-template/osx/VRL-Studio.app/Contents/MacOS/run.sh
@@ -13,12 +13,6 @@ CONF="-enable3d yes -resolution 1024x768 -defaultProject default.vrlp -property-
 
 OS=$(uname -a)
 
-#OSX_CONF="-Xdock:icon=../vrl-icon-osx.icns -Djava.library.path=custom-lib/osx -Xbootclasspath/p:lib/java3d/j3dcore.jar:lib/java3d/j3dutils.jar:lib/osx/jogl.jar:lib/java3d/vecmath.jar:lib/osx/gluegen-rt.jar -Dsun.boot.library.path=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Libraries:lib/osx -Dapple.laf.useScreenMenuBar=true"
-
-#OSX_CONF="-Xdock:icon=../vrl-icon-osx.icns -Dapple.laf.useScreenMenuBar=true -Djava.ext.dirs=lib/ext -cp lib/java3d/jogl-all.jar:lib/java3d/gluegen.jar:lib/java3d/gluegen-rt.jar:lib/java3d/j3dcore.jar:lib/java3d/j3dutils.jar:lib/java3d/vecmath.jar:lib/osx/gluegen-rt-natives-macosx-universal.jar:lib/osx/jogl-all-natives-macosx-universal.jar"
-
-OSX_CONF="-Xdock:name=VRL-Studio -Xdock:icon=../vrl-icon-osx.icns -Dapple.laf.useScreenMenuBar=true  -Djava.ext.dirs=."
-
 APPDIR="$(dirname "$0")/../Resources/.application"
 cd "$APPDIR"
 APPDIR="$(pwd)"
@@ -38,11 +32,6 @@ fi
 # ugly hack to enable vtk on osx
 export DYLD_LIBRARY_PATH="$HOME/.vrl/$VRL_VERSION/$PROPERTY_SUFFIX/plugins/VRL-VTK/natives/osx/:$DYLD_LIBRARY_PATH"
 
-# IF APPLICATION STUB SHALL BE USED
-#../../MacOS/JavaApplicationStub "$@"&
-#exit 0
-
-
 # IF BASH SCRIPT SHALL BE USED
 LIBDIR32="lib/linux/x86:custom-lib/linux/x86"
 LIBDIR64="lib/linux/x64:custom-lib/linux/x64"
@@ -53,20 +42,24 @@ then
 	echo ">> detected x86 (64 bit) os"
 	LIBDIR="$LIBDIR64:$LIBDIROSX"
 	JAVAEXE="jre/x64/bin/java"
+	JRE_EXT_DIR="jre/x64/lib/ext"
 elif [[ $OS == *x86_64* ]]
 then
   echo ">> detected x86 (64 bit) os"
   LIBDIR="$LIBDIR64:$LIBDIROSX"
   JAVAEXE="jre/x64/bin/java"
+	JRE_EXT_DIR="jre/x64/lib/ext"
 elif [[ $OS == *86* ]]
 then
   echo ">> detected x86 (32 bit) os"
   LIBDIR="$LIBDIR32:$LIBDIROSX"
   JAVAEXE="jre/x86/bin/java"
+	JRE_EXT_DIR="jre/x86/lib/ext"
 else
   echo ">> unsupported architecture!"
   echo " --> executing installed java version"
   JAVAEXE="java"
+	JRE_EXT_DIR="$JAVA_HOME/jre/lib/ext"
 fi
 
 if [ ! -e $JAVAEXE ]
@@ -74,9 +67,13 @@ then
   echo ">> integrated jre not found!"
   echo " --> executing installed java version"
   JAVAEXE="java"
+	JRE_EXT_DIR="$JAVA_HOME/jre/lib/ext"
 fi
 
-echo "PARAMS: $@"
+echo ">> specified parameters: $@"
+echo ">> ext-dir: $JRE_EXT_DIR"
+
+OSX_CONF="-Xdock:name=VRL-Studio -Xdock:icon=../vrl-icon-osx.icns -Dapple.laf.useScreenMenuBar=true -Djava.ext.dirs=$JRE_EXT_DIR"
 
 $JAVAEXE -Xms64m -Xmx4096m -splash:resources/studio-resources/splashscreen.png $OSX_CONF -jar VRL-Studio.jar $CONF "-file" "$@"
 
